7f2be640ec2bf0c74750b439d2a9e165b174fbed
CosmicSage <prabhakarmishra9@yahoo.com>
2023-10-14 13:57:01 +0530

    Browser

--- download.py
+++ download.py
@@ -11,15 +11,16 @@ target_url = "https://fiverr.com/kymmypops"  # Replace with the website URL you 
 # Define the domain you want to filter for
 target_domain = "fiverr-res.cloudinary.com"
 
-# Send an HTTP GET request to the target URL
-response = requests.get(target_url)
-
 # Define user agent and other browser-related headers
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
+"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+"Accept-Language": "en-US,en;q=0.5",
+"Accept-Encoding": "gzip, deflate, br",
 }
+
+# Send an HTTP GET request to the target URL
+response = requests.get(target_url, headers=headers)
+
 
 
 # Check if the request was successful
@@ -30,15 +31,15 @@ if True:
 
     # Find all the anchor tags (links) in the HTML
     links = soup.find_all('a')
-
+    titsout
     # Get the raw HTML content
     raw_html = response.text
 
     # Write the raw HTML content to a file
-    with open("raw_html_output.html", "w", encoding="utf-8") as file:
+    with open("out.html", "w", encoding="utf-8") as file:
         file.write(raw_html)
 
-    print("Raw HTML response has been written to 'raw_html_output.html'.")
+    print("Raw HTML response has been written to 'out.html'.")
 
     # Iterate through the links and filter by domain
     domain_links = [link.get('href') for link in links if link.get('href') and target_domain in urlparse(link.get('href')).netloc]
