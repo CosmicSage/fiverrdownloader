905788a5375853caf552943ff8172665c9550da0
CosmicSage <prabhakarmishra9@yahoo.com>
2023-10-14 13:44:58 +0530

    BotBlocked

--- download.py
+++ download.py
@@ -1,3 +1,50 @@
 #!/usr/bin/env python
+import requests
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
-print("down")
+# Define the target URL
+target_url = "https://fiverr.com/kymmypops"  # Replace with the website URL you want to scrape
+
+# Define the domain you want to filter for
+target_domain = "fiverr-res.cloudinary.com"
+
+# Send an HTTP GET request to the target URL
+response = requests.get(target_url)
+
+# Define user agent and other browser-related headers
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+}
+
+
+# Check if the request was successful
+if True:
+# if response.status_code == 200:
+    # Parse the HTML content of the page using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find all the anchor tags (links) in the HTML
+    links = soup.find_all('a')
+
+    # Get the raw HTML content
+    raw_html = response.text
+
+    # Write the raw HTML content to a file
+    with open("raw_html_output.html", "w", encoding="utf-8") as file:
+        file.write(raw_html)
+
+    print("Raw HTML response has been written to 'raw_html_output.html'.")
+
+    # Iterate through the links and filter by domain
+    domain_links = [link.get('href') for link in links if link.get('href') and target_domain in urlparse(link.get('href')).netloc]
+
+    # Print the filtered URLs
+    for link in domain_links:
+        print(link)
+else:
+    print("Failed to retrieve the web page. Status code:", response.status_code)
